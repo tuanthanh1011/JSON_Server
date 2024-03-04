2c82e1f018a60f55140d0340759c9246df5ee172
tuanthanh1011 <tuanthanh00200@gmail.com>
2024-03-04 15:44:14 +0700

    update version json-server

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8897; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+console.log("run app successfully");
